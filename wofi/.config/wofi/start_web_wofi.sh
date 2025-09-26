5178f65a594d42c283fc69999d394a40cbc5d780
xirzo <easttem@gmail.com>
2025-09-26 13:20:18 +0300

    feat: websearch with wofi

---
+++ wofi/.config/wofi/start_web_wofi.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if pgrep -x "wofi" >/dev/null; then
+    echo "Wofi is already running."
+    exit 1
+fi
+
+search=$(wofi --show dmenu --prompt "Search (g/d/w) <query>:")
+
+[[ -z "$search" ]] && exit
+
+prefix=$(echo "$search" | awk '{print $1}')
+query=$(echo "$search" | cut -d' ' -f2-)
+
+[[ -z "$query" ]] && exit
+
+case "$prefix" in
+    g)
+        url="https://www.google.com/search?q=$query"
+        ;;
+    d)
+        url="https://duckduckgo.com/?q=$query"
+        ;;
+    w)
+        url="https://en.wikipedia.org/wiki/Special:Search?search=$query"
+        ;;
+    *)
+        url="https://www.google.com/search?q=$search"
+        ;;
+esac
+
+xdg-open "$url"
