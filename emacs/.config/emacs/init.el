258793c7dbcafd2e34c67f7eabab02d66ab04b01
xirzo <easttem@gmail.com>
2026-01-27 12:37:45 +0300

    feat: add typst-preview and eaf browser

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -94,3 +94,51 @@
   :config
   (unless (treesit-language-available-p 'typst)
     (treesit-install-language-grammar 'typst)))
+
+(use-package eaf
+  :load-path "~/.config/emacs/lisp/emacs-application-framework"
+  :custom
+  ; See https://github.com/emacs-eaf/emacs-application-framework/wiki/Customization
+  (eaf-browser-continue-where-left-off t)
+  (eaf-browser-enable-adblocker t)
+  (browse-url-browser-function 'eaf-open-browser)
+  :config
+  (defalias 'browse-web #'eaf-open-browser)
+
+  (require 'eaf-pdf-viewer)
+  (require 'eaf-browser)
+
+  (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+  (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
+  (eaf-bind-key nil "M-q" eaf-browser-keybinding) ;; unbind, see more in the Wiki
+  ) 
+
+(use-package websocket)
+(use-package typst-preview
+  :load-path "~/.config/emacs/lisp/typst-preview/"
+  :init
+  (setq typst-preview-autostart t)
+  (setq typst-preview-open-browser-automatically t)
+
+  :custom
+  (typst-preview-browser "eaf-browser")
+  (typst-preview-invert-colors "auto")
+  (typst-preview-executable "/usr/sbin/tinymist")
+  (typst-preview-partial-rendering t)
+  
+  :config
+  (define-key typst-preview-mode-map (kbd "C-c C-j") 'typst-preview-send-position))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(eaf elixir-mode evil-collection gruber-darker-theme typst-preview
+	 typst-ts-mode websocket)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
