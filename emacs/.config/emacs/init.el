905ad70e640172630f90e09d88d8fe20dd4470ac
xirzo <easttem@gmail.com>
2026-01-27 12:09:27 +0300

    refactor: emacs config with use-package

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,82 +1,96 @@
-; Turn off gui stuff
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(add-to-list 'default-frame-alist '(undecorated . t))
-(setq inhibit-startup-message t)
-
-; Line numbers
-(setq display-line-numbers-type 'relative)
-(global-display-line-numbers-mode +1)
-
-; Dired multiple windows move
-(setq dired-dwim-target t)
-
-; Show whitespaces
-(add-hook 'diff-mode-hook (lambda ()
-                            (setq-local whitespace-style
-                                        '(face
-                                          tabs
-                                          tab-mark
-                                          spaces
-                                          space-mark
-                                          trailing
-                                          indentation::space
-                                          indentation::tab
-                                          newline
-                                          newline-mark))
-                            (whitespace-mode 1)))
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/")  t)
+(add-to-list 'package-archives
+             '(("melpa" . "https://melpa.org/packages/")
+               ("nongnu" . "https://elpa.nongnu.org/nongnu/"))
+             t)
+
 (package-initialize)
 
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+
+;; Ensure packages are installed by default
 (setq use-package-always-ensure t)
 
-(setq evil-want-keybinding nil)
+(use-package emacs
+  :ensure nil
+  :init
+  ;; Turn off gui stuff
+  (scroll-bar-mode -1)
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (setq inhibit-startup-message t)
+  
+  ;; Undecorated frame
+  (add-to-list 'default-frame-alist '(undecorated . t))
+  
+  ;; Font
+  (add-to-list 'default-frame-alist '(font . "Iosevka Nerd Font-17"))
 
-(unless package-archive-contents
-  (package-refresh-contents))
+  ;; Line numbers
+  (setq display-line-numbers-type 'relative)
+  (global-display-line-numbers-mode +1)
 
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
+  ;; Dired
+  (setq dired-dwim-target t)
 
-(unless (package-installed-p 'evil-collection)
-  (package-install 'evil-collection))
+  ;; Clipboard
+  (setq select-enable-clipboard t)
 
-(unless (package-installed-p 'gruber-darker-theme)
-  (package-install 'gruber-darker-theme))
+  ;; Visual line mode (Move by visual line, not logical line)
+  (global-visual-line-mode t))
 
-(load-theme 'gruber-darker t)
+;;; Diff / Whitespace Settings
+(use-package whitespace
+  :ensure nil
+  :hook (diff-mode . (lambda ()
+                       (setq-local whitespace-style
+                                   '(face
+                                     tabs
+                                     tab-mark
+                                     spaces
+                                     space-mark
+                                     trailing
+                                     indentation::space
+                                     indentation::tab 
+                                     newline 
+                                     newline-mark))
+                       (whitespace-mode 1))))
 
-(require 'evil)
-(evil-mode 1)
-(keymap-global-set "<escape>" 'keyboard-escape-quit)
+;;; Theme
+(use-package gruber-darker-theme
+  :config
+  (load-theme 'gruber-darker t))
 
-(require 'evil-collection)
-(evil-collection-init)
+;;; Evil Mode
+(use-package evil
+  :init
+  (setq evil-want-keybinding nil)
+  (setq evil-respect-visual-line-mode t)
+  :config
+  (evil-mode 1)
+  (keymap-global-set "<escape>" 'keyboard-escape-quit)
+  ;; Compilation shortcuts
+  (define-key evil-normal-state-map (kbd "C-c c") 'compile)
+  (define-key evil-normal-state-map (kbd "C-c r") 'recompile))
 
-; TODO: setup keybindings
-; (keymap-global-set "C-o" 'execute-extended-command )
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
 
-(setq select-enable-clipboard t)
+;;; Language Modes
+(use-package elixir-mode)
 
-
-(add-to-list 'default-frame-alist
-             '(font . "Iosevka Nerd Font-17"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
-     default))
- '(package-selected-packages '(evil-collection gruber-darker-theme)))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package typst-ts-mode
+  :mode "\\.typ\\'"
+  :init
+  (setq treesit-language-source-alist
+        '((typst "https://github.com/uben0/tree-sitter-typst")))
+  :config
+  (unless (treesit-language-available-p 'typst)
+    (treesit-install-language-grammar 'typst)))
