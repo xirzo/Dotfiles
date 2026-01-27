bffb45084a3ff9426b8284bfcf74d76c9bf2a024
xirzo <easttem@gmail.com>
2026-01-27 22:07:44 +0300

    chore: remove old i3 config

--- i3.bak/.config/i3/i3statusbar_layout.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-i3status | while :
-do
-    read line
-    layout=$(xkblayout-state print "%s")
-    echo ",[{\"full_text\":\"$layout\"},${line#?}" || exit 1
-done
--- i3.bak/.config/i3/set_mouse_accel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-xinput --set-prop "Logitech USB Receiver" "libinput Accel Speed" -1
-xinput --set-prop "SteelSeries SteelSeries Rival 3 Wireless" "libinput Accel Speed" -1
