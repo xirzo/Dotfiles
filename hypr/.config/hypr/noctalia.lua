e69e893c41aca049bf99fd643e5b362dc68ac9b7
xirzo <easttem@gmail.com>
2026-08-25 16:38:49 +0300

    feat(hypr): sync noctalia colors

--- hypr/.config/hypr/noctalia.lua
+++ hypr/.config/hypr/noctalia.lua
@@ -2,8 +2,12 @@
 
 local primary = "rgb(cba6f7)"
 local surface = "rgb(1e1e2e)"
+local on_surface = "rgb(cdd6f4)"
 local secondary = "rgb(fab387)"
+local on_secondary = "rgb(11111b)"
 local error = "rgb(f38ba8)"
+local on_error = "rgb(11111b)"
+local shadow = "rgb(11111b)"
 
 local function apply_theme()
     hl.config({
@@ -13,7 +17,14 @@ local function apply_theme()
                 inactive_border = surface,
             },
         },
-
+        decoration = {
+            shadow = {
+                color = shadow,
+            },
+            glow = {
+                color = shadow,
+            },
+        },
         group = {
             col = {
                 border_active = secondary,
@@ -23,12 +34,17 @@ local function apply_theme()
             },
 
             groupbar = {
+                gradients = true,
                 col = {
                     active = secondary,
                     inactive = surface,
                     locked_active = error,
                     locked_inactive = surface,
                 },
+                text_color = on_secondary,
+                text_color_inactive = on_surface,
+                text_color_locked_active = on_error,
+                text_color_locked_inactive = on_surface,
             },
         },
     })
@@ -38,8 +54,12 @@ return {
     colors = {
         primary = primary,
         surface = surface,
+        on_surface = on_surface,
         secondary = secondary,
+        on_secondary = on_secondary,
         error = error,
+        on_error = on_error,
+        shadow = shadow,
     },
     apply_theme = apply_theme
 }
